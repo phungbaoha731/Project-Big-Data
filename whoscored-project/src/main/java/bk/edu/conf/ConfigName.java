85ae70b2bcebf870f551c39cc11e99345a4a8a28
nguyenlthoe <nguyenlthoe@gmail.com>
2023-01-06 07:20:13 +0700

    update push

--- whoscored-project/src/main/java/bk/edu/conf/ConfigName.java
+++ whoscored-project/src/main/java/bk/edu/conf/ConfigName.java
@@ -1,7 +1,25 @@
 package bk.edu.conf;
 
 public class ConfigName {
-    public static final String PATH_RAW_DATA = "/user/data/day";
+    public static final String GK_OVERVIEW = "/user/gkOverview/day";
+
+    public static final String PLAYER_DEFENSIVE = "/user/playerDefensive/day";
+
+    public static final String PLAYER_MISCELLANEOUS = "/user/playerMiscellaneous/day";
+
+    public static final String PLAYER_OVERVIEW = "/user/playerOverview/day";
+
+    public static final String PLAYER_PASSING = "/user/playPassing/day";
+
+    public static final String PLAYER_PASS_TYPE = "/user/playPassType/day";
+
+    public static final String PLAYER_POSSESSION = "/user/playerPossession/day";
+
+    public static final String SHOT = "/user/shot/day";
+
+    public static final String TEAM_OVERVIEW = "/user/teamOverview/day";
 
     public static final String FORMAT_TIME = "dd-mm-yyyy";
+    
+    public static final String RESULT_MATCHES = "/user/resultMatches/day";
 }
--- whoscored-project/src/main/java/bk/edu/first/FirstPush.java
+++ whoscored-project/src/main/java/bk/edu/first/FirstPush.java
@@ -15,17 +15,90 @@ public class FirstPush implements Serializable {
         sparkUtil = new SparkUtil("who-scored", "save to hdfs", "yarn");
     }
 
-    private void saveData(){
+    private void pushData(){
         Dataset<Row> df = sparkUtil.getSparkSession().read()
                 .option("header", true)
                 .option("inferSchema", true)
-                .csv("file:///Data.csv");
+                .csv("file:/inputfile/gkOverview.csv");
         df.printSchema();
         df.show(3);
-        df.write().parquet(ConfigName.PATH_RAW_DATA + "/" + TimeUtil.getDate(ConfigName.FORMAT_TIME));
+        df.write().parquet(ConfigName.GK_OVERVIEW + "/" + TimeUtil.getDate(ConfigName.FORMAT_TIME));
+
+        df = sparkUtil.getSparkSession().read()
+                .option("header", true)
+                .option("inferSchema", true)
+                .csv("file:/inputfile/playerDefensive.csv");
+        df.printSchema();
+        df.show(3);
+        df.write().parquet(ConfigName.PLAYER_DEFENSIVE + "/" + TimeUtil.getDate(ConfigName.FORMAT_TIME));
+
+        df = sparkUtil.getSparkSession().read()
+                .option("header", true)
+                .option("inferSchema", true)
+                .csv("file:/inputfile/playerMiscellaneous.csv");
+        df.printSchema();
+        df.show(3);
+        df.write().parquet(ConfigName.PLAYER_MISCELLANEOUS + "/" + TimeUtil.getDate(ConfigName.FORMAT_TIME));
+
+        df = sparkUtil.getSparkSession().read()
+                .option("header", true)
+                .option("inferSchema", true)
+                .csv("file:/inputfile/playerOverview.csv");
+        df.printSchema();
+        df.show(3);
+        df.write().parquet(ConfigName.PLAYER_OVERVIEW + "/" + TimeUtil.getDate(ConfigName.FORMAT_TIME));
+        df = sparkUtil.getSparkSession().read()
+                .option("header", true)
+                .option("inferSchema", true)
+                .csv("file:/inputfile/playerPassing.csv");
+        df.printSchema();
+        df.show(3);
+        df.write().parquet(ConfigName.PLAYER_PASSING + "/" + TimeUtil.getDate(ConfigName.FORMAT_TIME));
+
+        df = sparkUtil.getSparkSession().read()
+                .option("header", true)
+                .option("inferSchema", true)
+                .csv("file:/inputfile/playerPassType.csv");
+        df.printSchema();
+        df.show(3);
+        df.write().parquet(ConfigName.PLAYER_PASS_TYPE+ "/" + TimeUtil.getDate(ConfigName.FORMAT_TIME));
+
+        df = sparkUtil.getSparkSession().read()
+                .option("header", true)
+                .option("inferSchema", true)
+                .csv("file:/inputfile/playerPossession.csv");
+        df.printSchema();
+        df.show(3);
+        df.write().parquet(ConfigName.PLAYER_POSSESSION + "/" + TimeUtil.getDate(ConfigName.FORMAT_TIME));
+
+        df = sparkUtil.getSparkSession().read()
+                .option("header", true)
+                .option("inferSchema", true)
+                .csv("file:/inputfile/shots.csv");
+        df.printSchema();
+        df.show(3);
+        df.write().parquet(ConfigName.SHOT + "/" + TimeUtil.getDate(ConfigName.FORMAT_TIME));
+
+        df = sparkUtil.getSparkSession().read()
+                .option("header", true)
+                .option("inferSchema", true)
+                .csv("file:/inputfile/teamOverview.csv");
+        df.printSchema();
+        df.show(3);
+        df.write().parquet(ConfigName.TEAM_OVERVIEW + "/" + TimeUtil.getDate(ConfigName.FORMAT_TIME));
+        
+        df = sparkUtil.getSparkSession().read()
+                .option("header", true)
+                .option("inferSchema", true)
+                .csv("file:/inputfile/result_matches.csv");
+        df.printSchema();
+        df.show(3);
+        df.write().parquet(ConfigName.RESULT_MATCHES + "/" + TimeUtil.getDate(ConfigName.FORMAT_TIME));
+
+
     }
     public static void main(String[] args){
         FirstPush firstPush = new FirstPush();
-        firstPush.saveData();
+        firstPush.pushData();
     }
 }
